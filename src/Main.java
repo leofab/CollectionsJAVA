e5790e5d6385bbd42ac413aa5f013c990516c372
Leo Fabricio <leofabr3@gmail.com>
2022-10-20 10:25:40 -0300

    iterator implement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
   public static void main(String[] args) throws Exception {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+
+    // Using iterators
+    Iterator<String> iterator = list.iterator();
+    while(iterator.hasNext()) {
+      String obj = iterator.next();
+      System.out.println(obj);
+    }
 
   }
 }
